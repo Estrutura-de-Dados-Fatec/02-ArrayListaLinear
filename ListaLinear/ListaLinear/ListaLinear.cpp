4695ddb88859b8c3b30d8b6ba9bc374c2e7df8ea
Claudio Nunes <claudio@metasistemas.com>
2022-08-23 18:30:27 -0300

    Ajuste no loop do menu (bug)

--- ListaLinear/ListaLinear/ListaLinear.cpp
+++ ListaLinear/ListaLinear/ListaLinear.cpp
@@ -25,7 +25,7 @@ int main()
 void menu()
 {
 	int op = 0;
-	while (op != 7) {
+	while (op != 6) {
 		system("cls"); // somente no windows
 		cout << "Menu Lista Linear";
 		cout << endl << endl;
@@ -101,6 +101,7 @@ void inserirElemento()
 
 }
 
+// deve ser implementada como resposta ao exercicio
 void buscarElemento()
 {
 
