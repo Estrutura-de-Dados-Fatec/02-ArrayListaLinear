9cc44a2a08a27ae8aa84a6e57f0471528ada38d7
Claudio Nunes <claudio@falecomseuprofessor.com.br>
2023-08-22 11:05:48 -0300

    Inclui meu nome

--- ListaLinear/ListaLinear/ListaLinear.cpp
+++ ListaLinear/ListaLinear/ListaLinear.cpp
@@ -1,3 +1,4 @@
+// Claudio Souza Nunes
 
 #include <iostream>
 using namespace std;
@@ -12,7 +13,7 @@ void buscarElemento();
 //--------------------------
 
 
-const int MAX = 2;;
+const int MAX = 5;
 int lista[MAX]{};
 int nElementos = 0;
 
